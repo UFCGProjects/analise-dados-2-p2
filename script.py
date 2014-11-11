5876a664217954dd5b7e7e7c2db3158720238487
Matheus Sampaio <matheus1401@gmail.com>
2014-11-10 21:26:38 -0300

    Finalizando problema 2

--- script.py
+++ script.py
@@ -23,47 +23,50 @@ def append(dic, term, title):
             dic[word][4] += 1
 
 def writeterm(csv, term, i):
-	
+
     csv.write(term)
-    
+
     total = 0
-    
+
     for word in dic:
         total += dic[word][i]
         csv.write(";%d" % dic[word][i])
-    
+
     csv.write(";%d\n" % total)
 
 def save(dic):
     csvOutput = open("csv/term_word_count.csv", "w")
 
-    csvOutput.write("word,rock,jazz,alternative rock,eletronic,pop,total\n")
+    csvOutput.write("word,rock,jazz,alternative rock,electronic,pop,total\n")
 
     for word in dic:
-        csvOutput.write("%s,%d,%d,%d,%d,%d,%d\n" % (word, dic[word][0], dic[word][1], dic[word][2], dic[word][3], dic[word][4], sum(dic[word])))
-    
-    csvOutput2 = open("csv/term_word_count4.csv", "w")
+        total =  float(sum(dic[word]))
 
-    csvOutput2.write("term");
-    
-    for word in dic:
-        csvOutput2.write(";%s" % word)
-    
-    csvOutput2.write(";total\n")
-    
-    writeterm(csvOutput2, "rock", 0)
-    writeterm(csvOutput2, "jazz",1)
-    writeterm(csvOutput2, "alternative rock", 2)
-    writeterm(csvOutput2, "electronic", 3)
-    writeterm(csvOutput2, "pop", 4) 
-    
-    
+        r = ("%s,%f,%f,%f,%f,%f,%f\n" % (word, dic[word][0], dic[word][1], dic[word][2], dic[word][3], dic[word][4], total))
+        csvOutput.write(r)
 
-    for word in dic:
-        csvOutput.write("%s,%d,%d,%d,%d,%d,%d\n" % (word, dic[word][0], dic[word][1], dic[word][2], dic[word][3], dic[word][4], sum(dic[word])))
+    # csvOutput2 = open("csv/term_word_count.csv", "w")
 
-    
-    csvOutput.close()
+    # csvOutput2.write("term");
+
+    # for word in dic:
+    #     csvOutput2.write(";%s" % word)
+
+    # csvOutput2.write(";total\n")
+
+    # writeterm(csvOutput2, "rock", 0)
+    # writeterm(csvOutput2, "jazz",1)
+    # writeterm(csvOutput2, "alternative rock", 2)
+    # writeterm(csvOutput2, "electronic", 3)
+    # writeterm(csvOutput2, "pop", 4)
+
+
+
+    # for word in dic:
+    #     csvOutput.write("%s,%d,%d,%d,%d,%d,%d\n" % (word, dic[word][0], dic[word][1], dic[word][2], dic[word][3], dic[word][4], sum(dic[word])))
+
+
+    # csvOutput.close()
 
 def unnacent(word):
     i = 0
@@ -80,7 +83,7 @@ def unnacent(word):
     return word
 
 dic = {}
- 
+
 def main():
     csvInput = open("csv/term_title.csv", "r")
 
